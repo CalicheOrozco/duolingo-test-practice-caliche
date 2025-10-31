283ccd3c752da718cd9d9192e3f89791c721dd20
CalicheOrozco <caliche2da1@gmail.com>
2025-10-31 05:28:14 -0300

    feat: create FillInTheBlanks component with Navbar and Footer integration

---
+++ src/pages/FillInTheBlanks.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import FillIntheBlanksComp from '../components/FillIntheBlanksComp'
+import Footer from '../components/Footer'
+
+function FillInTheBlanks() {
+  return (
+    <>
+    <Navbar />
+    <FillIntheBlanksComp />
+    <Footer />
+    </>
+  )
+}
+
+export default FillInTheBlanks
+
+
