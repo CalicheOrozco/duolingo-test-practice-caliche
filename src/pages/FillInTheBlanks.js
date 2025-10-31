10ab3323a90f82fdfbf41f6dccb77561a6699bef
CalicheOrozco <caliche2da1@gmail.com>
2025-10-31 14:38:25 -0300

    fix: correct casing in import statement for FillInTheBlanksComp component

--- src/pages/FillInTheBlanks.js
+++ src/pages/FillInTheBlanks.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import FillIntheBlanksComp from '../components/FillIntheBlanksComp'
+import FillInTheBlanksComp from '../components/FillInTheBlanksComp'
 import Footer from '../components/Footer'
-import FillIntheBlanksComp from '../components/FillIntheBlanksComp'
 
 function FillInTheBlanks() {
   return (
     <>
     <Navbar />
-    <FillIntheBlanksComp />
+    <FillInTheBlanksComp />
     <Footer />
     </>
   )
