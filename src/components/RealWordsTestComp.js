0e70ff915055668884e31c71ff40490751323684
CalicheOrozco <caliche2da1@gmail.com>
2025-11-09 22:14:37 -0400

    fix: disable exhaustive-deps rule for useEffect in RealWordsTestComp

--- src/components/RealWordsTestComp.js
+++ src/components/RealWordsTestComp.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import ReactCountdownClock from "react-countdown-clock";
